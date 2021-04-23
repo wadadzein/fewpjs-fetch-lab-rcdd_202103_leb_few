01103b3f3e6d1f92a946e9e5229176961850732c
wadad zein <wadad_zein@hotmail.com>
2021-04-23 22:50:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+//function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(1))
 
-function renderBooks(books) {
+.catch(function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
+
+})}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
