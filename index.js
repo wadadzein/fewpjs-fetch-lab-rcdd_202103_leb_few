e5bdf9149c43a06c149e43f9ed56439298ec46f9
wadad zein <wadad_zein@hotmail.com>
2021-04-23 22:53:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(data => renderBooks(data))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
