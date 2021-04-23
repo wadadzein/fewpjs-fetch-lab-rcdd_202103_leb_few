05e7933d6aa0d7427edba2508d5cc1db8e1b3bc0
wadad zein <wadad_zein@hotmail.com>
2021-04-23 22:51:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-//function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => console.log(1))
 
-.catch(function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(data => renderBooks(data))
+}
 
-})}
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
